55a98b1e5a00459e2102a0742a423912e1c875bf
raufodilov <applyjobrauf@gmail.com>
2021-05-21 13:00:53 +0800

    Sentiment Analysis with Naive Bayes

---
+++ Sentiment Analysis with Naive Bayes/Sentiment_Analysis_with_NB.py
@@ -0,0 +1,30 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# INTRODUCTION
+# We help simplify sentiment analysis using Python in this tutorial. 
+# You will learn how to build your own sentiment analysis classifier using Python and 
+# understand the basics of NLP (natural language processing).
+
+# loading necessary libraries
+import nltk
+from nltk.corpus import movie_reviews
+import random
+
+documents = [(list(movie_reviews.words(fileid)), category)
+              for category in movie_reviews.categories()
+              for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
+
+# Define the feature extractor
+
+all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+word_features = list(all_words)[:2000]
+
+def document_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains({})'.format(word)] = (word in document_words)
+    return features
+
+
