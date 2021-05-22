64066a5f38f4417eea8af75ed4a766a186fdf08c
raufodilov <applyjobrauf@gmail.com>
2021-05-22 12:29:40 +0800

    changes for improvement

--- Sentiment Analysis with Naive Bayes/Sentiment_Analysis_with_NB.py
+++ Sentiment Analysis with Naive Bayes/Sentiment_Analysis_with_NB.py
@@ -27,4 +27,24 @@ def document_features(document):
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
+# Train Naive Bayes classifier
+featuresets = [(document_features(d), c) for (d,c) in documents]
+train_set, test_set = featuresets[100:], featuresets[:100]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+# Test the classifier
+#_>print(nltk.classify.accuracy(classifier, test_set))# that is good our model evaluate test dataset with 74% accuracy
+
+# Show the most important features as interpreted by Naive Bayes
+#_>classifier.show_most_informative_features(5)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# CONCLUSION
+
+'''
+This is small sentiment analysis model for classifying text with the help of NLTK
+As classifier we use Naive Bayes which is common NLP classifier 
+'''
+
 
